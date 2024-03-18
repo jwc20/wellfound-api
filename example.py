8d93a726699741242af2d9c81a43d56193c8ebea
jwc20 <choij2015@gmail.com>
2024-03-18 20:58:04 +0900

    Create example file

---
+++ example.py
@@ -0,0 +1,16 @@
+import wellfound
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+username = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+
+client = wellfound.Wellfound(username=username, password=password)
+client.login()
+
+
+query = []
+client.get_companies(query=query)
