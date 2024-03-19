dac943a90f1766292a70dc99807981189ce3d996
jwc20 <choij2015@gmail.com>
2024-03-19 20:21:25 +0900

    Create base url and header

---
+++ wellfound/utils.py
@@ -0,0 +1,29 @@
+# import requests
+from enum import Enum
+# from bs4 import BeautifulSoup
+# import sys
+# import time
+
+
+# try:
+#     from urllib import urlencode, urlunsplit
+# except ImportError:
+#     from urllib.parse import urlunsplit, urlencode
+
+
+class Base(str, Enum):
+    URL = "https://www.wellfound.com/"
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Header(str, Enum):
+    PAYLOAD = {"Content-Type": "text/html; charset=UTF-8"}
+
+    def __str__(self):
+        return str(self.value)
+
+
+print(Base.URL)
+print(Header.PAYLOAD)
