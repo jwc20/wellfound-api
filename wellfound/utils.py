856fd5221d0a85b237d5d5044291016c20076f8e
jwc20 <choij2015@gmail.com>
2024-03-21 21:46:55 +0900

    Add method for checking login status

--- wellfound/utils.py
+++ wellfound/utils.py
@@ -30,6 +30,16 @@ class Header(str, Enum):
 #     def __str__(self):
 #         return str(self.URL)
 
+def check_login_status(self):
+    """Checks if the user is logged in."""
+    # self.driver.get(f"{Base.URL}/jobs")
+
+    if self.driver.current_url == f"{Base.URL}/jobs":
+        return True
+    # return self.wait_for_login_to_complete()
+    return False
+
+
 
 print(Base.URL)
 print(Header.PAYLOAD)
