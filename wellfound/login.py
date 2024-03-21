180073c788be69eed4fa63640fb3558c0a505ec0
jwc20 <choij2015@gmail.com>
2024-03-21 21:47:18 +0900

    Check login status before logging in

--- wellfound/login.py
+++ wellfound/login.py
@@ -19,15 +19,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
+from pprint import pprint
+
 
 class Login:
     def __init__(self, driver):
         self.driver = driver
 
+    # def check_login_status(self):
+    #     """Checks if the user is logged in."""
+    #     # self.driver.get(f"{Base.URL}/jobs")
+
+    #     if self.driver.current_url == f"{Base.URL}/jobs":
+    #         return True
+    #     # return self.wait_for_login_to_complete()
+    #     return False
+
     def navigate_to_login_page(self):
         """Navigates to the login page."""
         login_url = f"{Base.URL}/login"
-        print(login_url)  # Print the login URL for debugging purposes.
+        print(login_url)
         self.driver.get(login_url)
 
     def fill_login_form(self, email, password):
@@ -46,19 +57,54 @@ class Login:
     def wait_for_login_to_complete(self):
         """Waits for the login process to complete by checking for a specific element on the next page."""
         wait = WebDriverWait(self.driver, 10)
+        # print the current url
+        print(self.driver.current_url)
+        print(f"{Base.URL}/jobs")
+
         try:
             wait.until(EC.presence_of_element_located((By.ID, "nc_1_n1z")))
         except TimeoutException:
             print("Login page load timeout.")
+            # if self.driver.current_url == f"{Base.URL}/jobs":
+            #     return True
+
             return False
         return True
 
     def login(self, email, password):
         """Performs the complete login operation."""
-        self.navigate_to_login_page()
-        self.fill_login_form(email, password)
-        self.submit_login_form()
-        if self.wait_for_login_to_complete():
-            print("Successfully logged in.")
-        else:
-            print("Failed to log in.")
+
+
+
+        # from utils.py
+        # if check_login_status(self):
+        #     print("Already logged in.")
+        #     return
+
+        try:
+            login_status = check_login_status(self)
+            if login_status:
+                print("Already logged in.")
+                return
+            # else login
+            self.navigate_to_login_page()
+            self.fill_login_form(email, password)
+            self.submit_login_form()
+            if self.wait_for_login_to_complete():
+                print("Successfully logged in.")
+                pprint(self.driver.get_cookies())
+            else:
+                print("Failed to log in.")
+        except:
+            print("Failed to check login status.")
+            return
+
+
+        # self.navigate_to_login_page()
+        # self.fill_login_form(email, password)
+        # self.submit_login_form()
+        # if self.wait_for_login_to_complete():
+        #     print("Successfully logged in.")
+        #     pprint(self.driver.get_cookies())
+        # else:
+        #     print("Failed to log in.")
