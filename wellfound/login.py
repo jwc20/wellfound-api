4a0241e5f7d0a353a9e601960679a93f5052eb92
jwc20 <choij2015@gmail.com>
2024-03-23 15:31:57 +0900

    Decompose

--- wellfound/login.py
+++ wellfound/login.py
@@ -1,10 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
@@ -25,29 +21,39 @@ from pprint import pprint
 class Login:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(self.driver, 5)
+
 
     def check_login_status(self):
         """Checks if the user is logged in by checking the url."""
-        wait = WebDriverWait(self.driver, 5)
-        
         # initial page: chrome://new-tab-page/
         # get the current url after redirect from chrome://new-tab-page/
-        self.driver.get(f"{Base.URL}/jobs")
-        time.sleep(5)
+        self.navigate_to_jobs_page()
 
         try:
-            # wait until the element is loaded in the page where data-testid="JobSearchPage"
-            wait.until(EC.presence_of_element_located((By.XPATH, '//*[@data-test="JobSearchPage"]')))
+            # wait until the element is loaded in the page where data-test="JobSearchPage"
+            self.wait.until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '//*[@data-test="JobSearchPage"]')
+                )
+            )
         except TimeoutException:
             print("Jobs page load timeout.")
             return False
         return True
 
+    def navigate_to_jobs_page(self):
+        """Navigates to the jobs page."""
+        jobs_url = f"{Base.URL}/jobs"
+        self.driver.get(jobs_url)
+        time.sleep(5)
+
     def navigate_to_login_page(self):
         """Navigates to the login page."""
         login_url = f"{Base.URL}/login"
-        print(login_url)
+        # print(login_url)
         self.driver.get(login_url)
+        time.sleep(5)
 
     def fill_login_form(self, email, password):
         """Fills in the login form with email and password."""
@@ -64,13 +70,12 @@ class Login:
 
     def wait_for_login_to_complete(self):
         """Waits for the login process to complete by checking for a specific element on the next page."""
-        wait = WebDriverWait(self.driver, 10)
         # print the current url
         print(self.driver.current_url)
-        print(f"{Base.URL}/jobs")
+        print(f"{Base.URL}/logins")
 
         try:
-            wait.until(EC.presence_of_element_located((By.ID, "user_email")))
+            self.wait.until(EC.presence_of_element_located((By.ID, "user_email")))
         except TimeoutException:
             print("Login page load timeout.")
             return False
@@ -82,7 +87,7 @@ class Login:
             if self.check_login_status():
                 print("Already logged in.")
                 return
-            else: 
+            else:
                 print("Not logged in.")
                 self.navigate_to_login_page()
                 self.fill_login_form(email, password)
