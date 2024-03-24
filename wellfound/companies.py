e15cd0c5055a1c9cd4a7e724bb9d661897b72ab4
jwc20 <choij2015@gmail.com>
2024-03-24 12:57:47 +0900

    Implement a way to scrape multiple pages
    Need to get the number of pages

--- wellfound/companies.py
+++ wellfound/companies.py
@@ -64,111 +64,116 @@ class Companies:
         soup = BeautifulSoup(self.driver.page_source, "lxml")
         # pprint(soup.prettify())
 
-        # Search with custom query
 
-        # JavaScript code to execute a POST request
-        js_script = """
-        var callback = arguments[0];
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'https://wellfound.com/graphql?fallbackAOR=talent', true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.setRequestHeader('Accept', '*/*');
-        xhr.setRequestHeader('Accept-Encoding', 'gzip, deflate, br, zstd'); // Note: Browsers might not allow JavaScript to set this header
-        xhr.setRequestHeader('Accept-Language', 'en-US,en;q=0.9');
-        xhr.setRequestHeader('Apollographql-Client-Name', 'talent-web');
-        xhr.setRequestHeader('Origin', 'https://wellfound.com');
-        xhr.setRequestHeader('Referer', 'https://wellfound.com/jobs');
-        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 
-        // User-Agent and Cookies are typically managed by the browser and not settable via XMLHttpRequest
+        for i in range(1, 2):
+            # JavaScript code to execute a POST request
+            js_script = """
+            var callback = arguments[0];
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'https://wellfound.com/graphql?fallbackAOR=talent', true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.setRequestHeader('Accept', '*/*');
+            xhr.setRequestHeader('Accept-Encoding', 'gzip, deflate, br, zstd');
+            xhr.setRequestHeader('Accept-Language', 'en-US,en;q=0.9');
+            xhr.setRequestHeader('Apollographql-Client-Name', 'talent-web');
+            xhr.setRequestHeader('Origin', 'https://wellfound.com');
+            xhr.setRequestHeader('Referer', 'https://wellfound.com/jobs');
+            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    callback(xhr.responseText);
-                } else {
-                    console.error("Error with status code:", xhr.status);
-                    callback("Error: " + xhr.statusText);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        callback(xhr.responseText);
+                    } else {
+                        console.error("Error with status code:", xhr.status);
+                        callback("Error: " + xhr.statusText);
+                    }
                 }
-            }
-        };
-        xhr.send(JSON.stringify({
-            "operationName": "JobSearchResultsX",
-            "variables": {
-                "filterConfigurationInput": {
-                "page": 1,
-                "remoteCompanyLocationTagIds": [
-                    "1692", 
-                    "1693"
-                ],
-                "roleTagIds": [
-                    "14726"
-                ],
-                "skillTagIds": [
-                    "14775",
-                    "139914",
-                    "17000"
-                ],
-                "equity": {
-                    "min": null,
-                    "max": null
+            };
+            xhr.send(JSON.stringify({
+                "operationName": "JobSearchResultsX",
+                "variables": {
+                    "filterConfigurationInput": {
+                    "page": """ + str(i) + """,
+                    "remoteCompanyLocationTagIds": [
+                        "1692", 
+                        "1693"
+                    ],
+                    "roleTagIds": [
+                        "14726"
+                    ],
+                    "skillTagIds": [
+                        "14775",
+                        "139914",
+                        "17000"
+                    ],
+                    "equity": {
+                        "min": null,
+                        "max": null
+                    },
+                    "excludedKeywords": [
+                        "web3",
+                        "crypto",
+                        "cryptocurrency"
+                    ],
+                    "jobTypes": [
+                        "full_time"
+                    ],
+                    "remotePreference": "REMOTE_OPEN",
+                    "salary": {
+                        "min": null,
+                        "max": null
+                    },
+                    "yearsExperience": {
+                        "max": null,
+                        "min": null
+                    }
+                    }
                 },
-                "excludedKeywords": [
-                    "web3",
-                    "crypto",
-                    "cryptocurrency"
-                ],
-                "jobTypes": [
-                    "full_time"
-                ],
-                "remotePreference": "REMOTE_OPEN",
-                "salary": {
-                    "min": null,
-                    "max": null
-                },
-                "yearsExperience": {
-                    "max": null,
-                    "min": null
+                "extensions": {
+                    "operationId": "tfe/b898ee628dd3385e1b8c467e464a0261ad66c478eda6e21e10566b0ca4ccf1e9"
                 }
-                }
-            },
-            "extensions": {
-                "operationId": "tfe/b898ee628dd3385e1b8c467e464a0261ad66c478eda6e21e10566b0ca4ccf1e9"
-            }
-        }));
-        """
+            }));
+            """
 
 
 
-        # Execute the JavaScript
-        response = self.driver.execute_async_script(js_script)
+            # Execute the JavaScript
+            response = self.driver.execute_async_script(js_script)
 
-        time.sleep(5)
-        # pprint(response)
+            time.sleep(5)
+            # pprint(response)
 
-        # Parse the JSON response
-        response = json.loads(response)
-        
-        # convert the response json 
-        response = json.dumps(response, indent=4)
+            # Parse the JSON response
+            response = json.loads(response)
+            
+            # convert the response json 
+            response = json.dumps(response, indent=4)
 
-        # save the response to a file
-        with open(f"response_{date_time_format}.json", "w") as file:
-            print("writing to file...")
-            file.write(response)
+            # save the response to a file
+            # with open(f"response_{date_time_format}.json", "w") as file:
+            # include the page number in the filename
+            with open(f"response_{date_time_format}_page_{i}.json", "w") as file:
+                print("writing to file...")
+                file.write(response)
 
 
         # time.sleep(1000)
 
-        # TODO: SCRAPE THE PAGE
 
         # save the soup to a file
-        with open(f"companies_{date_time_format}.html", "w") as file:
-            print("writing to file...")
-            file.write(soup.prettify())
+        # with open(f"companies_{date_time_format}.html", "w") as file:
+        #     print("writing to file...")
+        #     file.write(soup.prettify())
 
-        print("sleeping for 10 seconds...")
-        time.sleep(10)
+        # print("sleeping for 10 seconds...")
+        # time.sleep(10)
+        for remaining in range(10, 0, -1):
+            sys.stdout.write("\rsleeping in {:2d} seconds...".format(remaining))
+            sys.stdout.flush()
+            time.sleep(1)
+        sys.stdout.write("\rComplete!                       \n")
 
         # results = self._scrape_companies(soup)
 
